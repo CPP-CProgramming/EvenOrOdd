b7f7d6fb33f18e8ebf412a6619359de59f6188fd
In Chan Hwang <neogeo-s@hanmail.net>
2017-08-27 17:59:25 +0900

    Add project files.

---
+++ EvenOrOdd/frnEvenorOdd.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace EvenOrOdd
+{
+    partial class evenOrOdd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // evenOrOdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "evenOrOdd";
+            this.Text = "Even or Odd";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
